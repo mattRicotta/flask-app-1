4c6dcdb573d14bf54612ed9f81a69f0eef7d1f2f
mattRicotta <mattRicotta@fakeemail.com>
2021-03-13 13:40:00 -0500

    change helper.py to resume_data.py

---
+++ app.py
@@ -0,0 +1,4 @@
+from flask import Flask, render_template
+from resume_data import about, work_experience, education, certifications, publications, skills, hobbies
+
+print(about)
---
+++ resume_data.py
@@ -0,0 +1,163 @@
+# Use this file to maintain resume data
+# ======================================================
+# ABOUT
+about = 'Matt Licata has been in the craft brewing industry for 10 years and has 5 years of experience in QA/QC and lab management. He has a true passion for implementing technology to facilitate data-driven decision making.'
+# ======================================================
+# WORK EXPERIENCE
+"""
+work_experience dictionary format:
+  {
+    'title' : '', 
+    'company' : '', 
+    'city' : '', 
+    'start_date' : 'mmm yyyy', 
+    'end_date' : 'mmm yyyy', 
+    'description' : [
+      'list of highlights'
+    ]
+  }
+"""
+work_experience = [
+  {
+    'title' : 'Lead Quality Analyst', 
+    'company' : 'Portland Brewing Company (FIFCO USA)', 
+    'city' : 'Portland, OR', 
+    'start_date' : 'Jan 2015', 
+    'end_date' : 'Oct 2020', 
+    'description' : [
+      'Developed a relational database for the lab that exponentially improved QA team’s overall capability, improved data quality and eliminated 20-person-hours per week of redundant data entry, data cleanup, and report building.', 
+      'Created front-end applications for other departments to provide with up-to-date, dynamic reports as well as tools to search records, export data, and execute common brewery calculations.', 
+      'Implemented micro and quality risk tracking capability within the database which eliminated recurring issues and saved over $100,000 per year in potential product loss.', 
+      'Managed sensory panel of 10 panelists, organized and administered regular training sessions, and implemented and provided training with DraughtLab Pro.', 
+      'Trained, supervised, and mentored 2 quality analysts with one becoming the resident microbiologist and the other eventually overseeing production QC and administering sensory panel.'
+    ]
+  }, 
+  {
+    'title' : 'Head Brewer', 
+    'company' : 'Fort George Brewery', 
+    'city' : 'Astoria, OR', 
+    'start_date' : 'Dec 2013', 
+    'end_date' : 'Oct 2014', 
+    'description' : [
+      'Supervised, trained, and mentored a team of 7 brewers with several going on to win awards for recipes, and one brewer moving up to manage the barrel aging program.', 
+      'Helped increase production capacity by 33% through equipment purchases and process improvements.'
+      'Orchestrated microbiology education and training that improved yeast management and product handling, which saved $25,000-50,000 per year in potential product losses.', 
+      'Collaborated with marketing team to establish reliable forecasting for production planning and reduce surplus by 20%.', 
+      'Implemented regular training and education for restaurant staff on beer styles, brewing science, and sensory analysis which enabled them to provide knowledgeable service to customers and constructive feedback to the brewery.'
+    ]
+  }, 
+  {
+    'title' : 'Brewer', 
+    'company' : 'Dogfish Head Brewery', 
+    'city' : 'Milton, DE', 
+    'start_date' : 'Jul 2011', 
+    'end_date' : 'Oct 2013', 
+    'description' : [
+      'Operated brewery equipment in all stages of production from brewhouse to clarification in a 100,000 BBL/year brewery.', 
+      'Developed over 40 new SOPs during a $56M expansion project.', 
+      'Consulted with engineering team in the design and installation of $2M yeast cellaring system and gathered and analyzed data to optimize ABER meter calibration and yeast propagation and pitching methods.'
+    ]
+  }
+]
+# ======================================================
+# EDUCATION
+"""
+education dictionary format:
+  {
+    'degree': '', 
+    'major': '', 
+    'school': '',
+    'location': '',  
+    'dates': ''
+  }
+"""
+education = [
+  {
+    'degree': 'Diploma', 
+    'major': 'Brewing Science and Engineering', 
+    'school': 'American Brewers Guild',
+    'location': 'Salisbury, VT',  
+    'dates': 'Jan - Jun 2011'
+  }, 
+  {
+    'degree': 'Master of Science', 
+    'major': 'Animal Science - Virology', 
+    'school': 'University of Delaware', 
+    'location': 'Newark, DE',  
+    'dates': '2004 - 2007'
+  }, 
+  {
+    'degree': 'Bachelor of Science', 
+    'major': 'Animal Science - Agricultural Biotechnology', 
+    'school': 'University of Delaware',
+    'location': 'Newark, DE',  
+    'dates': '2000-2004'
+  }
+]
+# ======================================================
+# CERTIFICATIONS
+"""
+certifications dictionary format:
+  {
+    'certification': '', 
+    'institution': '',
+    'issued_on': 'mmm yyyy'
+  }
+"""
+certifications = [
+  {
+    'certification': 'Salesforce Certified Administrator', 
+    'institution': 'Salesforce.com',
+    'issued_on': 'mmm yyyy'
+  },   
+  {
+    'certification': 'Sensory Panel Management', 
+    'institution': 'Seibel Institute of Technology',
+    'issued_on': 'Nov 2017'
+  }
+]
+# ======================================================
+# PUBLICATIONS
+"""
+publication format:
+  {
+    'title': '',
+    'authors': '',
+    'journal': '',
+    'pub_date': '',
+    'url': '',
+    'url_label': ''
+  }
+"""
+publications = [
+  {
+    'title': 'Tumor suppressor and oncogene actions of TGFbeta1 occur early in skin carcinogenesis and are mediated by Smad3',
+    'authors': 'DS Bae, N Blazanin, M Licata, J Lee, AB Glick',
+    'journal': 'Molecular Carcinogenesis',
+    'pub_date': 'May 2009',
+    'url': 'https://pubmed.ncbi.nlm.nih.gov/18942075/',
+    'url_label': 'PubMed ID: 18942075'
+  },
+  {
+    'title': 'Evaluating viral interference between infectious bronchitis virus and Newcastle disease virus vaccine strains using quantitative reverse transcription-polymerase chain reaction',
+    'authors': 'J Gelb Jr, BS Ladman, MJ Licata, MH Shapiro, LR Campion',
+    'journal': 'Avian Diseases',
+    'pub_date': 'Dec 2007',
+    'url': 'https://pubmed.ncbi.nlm.nih.gov/18251404/',
+    'url_label': 'PubMed ID: 18251404'
+  }
+]
+# ======================================================
+# SKILLS
+skills = [
+  ['Leadership', 'Communication', 'Teaching', 'Data Visualization'], 
+  ['Root Cause Analysis', 'Statistical Analysis', 'Database Design', 'Data Management'], 
+  ['VBA', 'SQL', 'Python', 'HTML/CSS']
+]
+# ======================================================
+# HOBBIES
+hobbies = [
+  ['Cooking', 'Surfing', 'Hiking', 'Sci-fi Literature'], 
+  ['PC Building', 'Arduino', 'Coding'], 
+  ['Costume Making', 'Live Music', 'Playing Music']
+]
