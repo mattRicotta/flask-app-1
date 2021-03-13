88dd58017875318b95fd4eb1a6fbdfebcb3e5fe7
mattRicotta <mattRicotta@fakeemail.com>
2021-03-13 14:59:25 -0500

    first draft completed

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 from flask import Flask, render_template
 from resume_data import about, work_experience, education, certifications, publications, skills, hobbies
 
-print(about)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template(
+    "index.html", 
+    about=about, 
+    work_experience=work_experience, 
+    education=education, 
+    certifications=certifications, 
+    publications=publications, 
+    skills=skills, 
+    hobbies=hobbies
+  )
