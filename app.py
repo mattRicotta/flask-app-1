11c67eca73a2beefa04c571459c65a412aee4318
mattricotta <orangecharlie154@gmail.com>
2021-03-19 15:27:31 -0400

    update app.py change resume_data.py to resume.py update stylesheet.css .column width update index.html

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from resume_data import about, work_experience, education, certifications, publications, skills, hobbies
+from resume import about, work_experience, education, certifications, publications, skills, hobbies
 
 app = Flask(__name__)
 
--- resume.py
+++ resume.py
@@ -1,7 +1,9 @@
 # Use this file to maintain resume data
 # ======================================================
 # ABOUT
-about = 'Matt Licata has been in the craft brewing industry for 10 years and has 5 years of experience in QA/QC and lab management. He has a true passion for implementing technology to facilitate data-driven decision making.'
+about = ('Matt Licata has been in the craft brewing industry for 10 years and has 5 years of experience in '
+         'QA/QC and lab management. He has a true passion for implementing technology to facilitate '
+         'data-driven decision making.')
 # ======================================================
 # WORK EXPERIENCE
 """
@@ -13,7 +15,7 @@ work_experience dictionary format:
     'start_date' : 'mmm yyyy', 
     'end_date' : 'mmm yyyy', 
     'description' : [
-      'list of highlights'
+      ('')
     ]
   }
 """
@@ -25,11 +27,17 @@ work_experience = [
     'start_date' : 'Jan 2015', 
     'end_date' : 'Oct 2020', 
     'description' : [
-      'Developed a relational database for the lab that exponentially improved QA team’s overall capability, improved data quality and eliminated 20-person-hours per week of redundant data entry, data cleanup, and report building.', 
-      'Created front-end applications for other departments to provide with up-to-date, dynamic reports as well as tools to search records, export data, and execute common brewery calculations.', 
-      'Implemented micro and quality risk tracking capability within the database which eliminated recurring issues and saved over $100,000 per year in potential product loss.', 
-      'Managed sensory panel of 10 panelists, organized and administered regular training sessions, and implemented and provided training with DraughtLab Pro.', 
-      'Trained, supervised, and mentored 2 quality analysts with one becoming the resident microbiologist and the other eventually overseeing production QC and administering sensory panel.'
+      ('Developed a relational database for the lab that exponentially improved QA team’s overall capability, '
+       'improved data quality and eliminated 20-person-hours per week of redundant data entry, data cleanup, '
+       'and report building.'), 
+      ('Created front-end applications for other departments to provide with up-to-date, dynamic reports as '
+       'well as tools to search records, export data, and execute common brewery calculations.'), 
+      ('Implemented micro and quality risk tracking capability within the database which eliminated recurring '
+       'issues and saved over $100,000 per year in potential product loss.'), 
+      ('Managed sensory panel of 10 panelists, organized and administered regular training sessions, and '
+       'implemented and provided training with DraughtLab Pro.'), 
+      ('Trained, supervised, and mentored 2 quality analysts with one becoming the resident microbiologist '
+       'and the other eventually overseeing production QC and administering sensory panel.')
     ]
   }, 
   {
@@ -39,11 +47,16 @@ work_experience = [
     'start_date' : 'Dec 2013', 
     'end_date' : 'Oct 2014', 
     'description' : [
-      'Supervised, trained, and mentored a team of 7 brewers with several going on to win awards for recipes, and one brewer moving up to manage the barrel aging program.', 
-      'Helped increase production capacity by 33% through equipment purchases and process improvements.'
-      'Orchestrated microbiology education and training that improved yeast management and product handling, which saved $25,000-50,000 per year in potential product losses.', 
-      'Collaborated with marketing team to establish reliable forecasting for production planning and reduce surplus by 20%.', 
-      'Implemented regular training and education for restaurant staff on beer styles, brewing science, and sensory analysis which enabled them to provide knowledgeable service to customers and constructive feedback to the brewery.'
+      ('Supervised, trained, and mentored a team of 7 brewers with several going on to win awards for recipes, '
+       'and one brewer moving up to manage the barrel aging program.'), 
+      ('Helped increase production capacity by 33% through equipment purchases and process improvements.'),
+      ('Orchestrated microbiology education and training that improved yeast management and product handling, '
+       'which saved $25,000-50,000 per year in potential product losses.'), 
+      ('Collaborated with marketing team to establish reliable forecasting for production planning and reduce '
+       'surplus by 20%.'), 
+      ('Implemented regular training and education for restaurant staff on beer styles, brewing science, and '
+       'sensory analysis which enabled them to provide knowledgeable service to customers and constructive '
+       'feedback to the brewery.')
     ]
   }, 
   {
@@ -53,9 +66,12 @@ work_experience = [
     'start_date' : 'Jul 2011', 
     'end_date' : 'Oct 2013', 
     'description' : [
-      'Operated brewery equipment in all stages of production from brewhouse to clarification in a 100,000 BBL/year brewery.', 
-      'Developed over 40 new SOPs during a $56M expansion project.', 
-      'Consulted with engineering team in the design and installation of $2M yeast cellaring system and gathered and analyzed data to optimize ABER meter calibration and yeast propagation and pitching methods.'
+      ('Operated brewery equipment in all stages of production from brewhouse to clarification in a '
+       '100,000 BBL/year brewery.'), 
+      ('Developed over 40 new SOPs during a $56M expansion project.'), 
+      ('Consulted with engineering team in the design and installation of $2M yeast cellaring system '
+       'and gathered and analyzed data to optimize ABER meter calibration and yeast propagation and '
+       'pitching methods.')
     ]
   },
     {
@@ -65,9 +81,9 @@ work_experience = [
     'start_date' : 'Jun 2010', 
     'end_date' : 'Jul 2011', 
     'description' : [
-      'Operated, organized, and cleaned cold-side, sauté, and grill stations.', 
-      'Developed recipes for seasonal menu items.', 
-      'Trained new hires on prep and service tasks.'
+      ('Operated, organized, and cleaned cold-side, sauté, and grill stations.'), 
+      ('Developed recipes for seasonal menu items.'), 
+      ('Trained new hires on prep and service tasks.')
     ]
   },
     {
@@ -77,9 +93,9 @@ work_experience = [
     'start_date' : 'Oct 2006', 
     'end_date' : 'May 2010', 
     'description' : [
-      'Contributed to research projects with resulting in a successful publication in Molecular Carcinogenesis', 
-      'Oversaw laboratory operations, safety compliance, and purchasing/inventory.', 
-      'Managed mouse colony and maintained transgenic breeder lines.'
+      ('Contributed to research projects with resulting in a successful publication in Molecular Carcinogenesis'), 
+      ('Oversaw laboratory operations, safety compliance, and purchasing/inventory.'), 
+      ('Managed mouse colony and maintained transgenic breeder lines.')
     ]
   }
 ]
@@ -163,7 +179,8 @@ publications = [
     'url_label': 'PubMed ID: 18942075'
   },
   {
-    'title': 'Evaluating viral interference between infectious bronchitis virus and Newcastle disease virus vaccine strains using quantitative reverse transcription-polymerase chain reaction',
+    'title': ('Evaluating viral interference between infectious bronchitis virus and Newcastle disease virus vaccine '
+              'strains using quantitative reverse transcription-polymerase chain reaction'),
     'authors': 'J Gelb Jr, BS Ladman, MJ Licata, MH Shapiro, LR Campion',
     'journal': 'Avian Diseases',
     'pub_date': 'Dec 2007',
@@ -174,7 +191,7 @@ publications = [
 # ======================================================
 # SKILLS
 skills = [
-  ['Leadership', 'Communication', 'Teaching', 'Data Visualization'], 
+  ['Leadership', 'Communication', 'Teaching', 'Data Visualization'],
   ['Root Cause Analysis', 'Statistical Analysis', 'Database Design', 'Data Management'], 
   ['VBA', 'SQL', 'Python', 'HTML/CSS']
 ]
